8d6ac873444bc4d9cab0c42480f37b30f575ff5f
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-09-12 21:13:36 -0400

    Registro de Analisis, Completando Estructura

---
+++ DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Analisis> Analisi { get; set; }
+        public DbSet<TiposAnalisis> TipoAnalisis { get; set; }
+        public DbSet<Pacientes> Paciente { get; set; }
+
+        public Contexto() : base("Constr")
+        {
+            
+        }
+    }
+}
